65b36081a829439dc8e6eeae39fc4a536d35ffd1
Farhoud Khoshnoud <farhoudkhf@gmail.com>
2024-10-22 22:44:39 -0700

    first commit

---
+++ testPy.py
@@ -0,0 +1,14 @@
+def xAdd(x, y):
+    return x + y
+
+def xMulti(x, y):
+    return x * y
+
+def xPower(x, y):
+    return x ** y
+
+x = 2
+y = 8
+print(f"thi is xAdd: {xAdd(x, y)} endPrint.")
+print(f"thi is xMulti: {xMulti(x, y)} endPrint.")
+print(f"thi is xPower: {xPower(x, y)} endPrint.")
